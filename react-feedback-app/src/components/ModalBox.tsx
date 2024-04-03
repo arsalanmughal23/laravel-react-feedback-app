847603e924b51c1fc2e19128088d931edd3de71d
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-03 15:59:35 +0500

    Modify ModalBox Improve (Experience, Cases, PropsHandling)   - Reusable-Experience   - Complex-Cases   - Handling-With-Props

--- react-feedback-app/src/components/ModalBox.tsx
+++ react-feedback-app/src/components/ModalBox.tsx
@@ -8,34 +8,77 @@ import {
     TEModalFooter,
 } from "tw-elements-react";
 import { themeClass } from "../constants/constants";
-import Button from "./Button";
+import Button, { ButtonPropsType } from "./Button";
+import { FormProps } from "react-router-dom";
 
-interface ModalBox {
+interface ModalBoxPropsType {
     showModal: boolean,
     setShowModal: Function,
-    title: string,
     children: ReactElement,
     isCenter?: boolean,
     position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right',
     size?: 'sm' | 'lg' | 'xl' | 'fullscreen',
     restrictBackDrop?: boolean,
+    formProps?: FormProps | null
+}
+interface ModalBoxContentPropsType {
+    title: string,
+    setShowModal: Function,
     theme?: 'primary' | 'secondary',
-    handleAction?: Function
+    children: ReactElement,
+    footerActions?: ButtonPropsType[]
 }
 
-export default function ModalBox({
-    showModal = false,
-    setShowModal,
-    title,
-    isCenter = false,
-    position = 'top-right',
-    size = 'lg',
-    restrictBackDrop = false,
-    children,
-    theme = 'primary',
-    handleAction = () => {}
-}: ModalBox) {
+export default function ModalBox({ ...props }: ModalBoxPropsType & ModalBoxContentPropsType) {
 
+    let {
+        showModal = false,
+        setShowModal,
+        isCenter = false,
+        position = 'top-right',
+        size = 'lg',
+        restrictBackDrop = false,
+        children,
+        formProps,
+    } = { ...props };
+
+    return (
+        <>
+            {/* Modal */}
+            <TEModal show={showModal} setShow={setShowModal} staticBackdrop={restrictBackDrop} className={`${isCenter && 'flex items-center'}`}>
+                <TEModalDialog position={position} className={`${isCenter && 'left-0'} p-5`} size={size}
+                    theme={{
+                        show: "translate-x-0 opacity-100",
+                        hidden: "translate-x-[100%] opacity-0",
+                    }}
+                >
+                    <TEModalContent>
+                        { formProps ? (
+                            <form {...formProps} >
+                                <ModalBoxContent {...props} >
+                                    { children }
+                                </ModalBoxContent>
+                            </form>
+                        ) : (
+                            <ModalBoxContent {...props} >
+                                { children }
+                            </ModalBoxContent>
+                        )}
+                    </TEModalContent>
+                </TEModalDialog>
+            </TEModal>
+        </>
+    );
+}
+
+const ModalBoxContent = ({
+    title,
+    setShowModal,
+    theme,
+    children,
+    footerActions = []
+}:ModalBoxContentPropsType) => {
+    
     let headerClasses = "";
     let headerTitleClasses = "";
 
@@ -53,53 +96,39 @@ export default function ModalBox({
 
     return (
         <>
-            {/* Modal */}
-            <TEModal show={showModal} setShow={setShowModal} staticBackdrop={restrictBackDrop} className={`${isCenter && 'flex items-center'}`}>
-                <TEModalDialog position={position} className={`${isCenter && 'left-0'} p-5`} size={size}
-                    theme={{
-                        show: "translate-x-0 opacity-100",
-                        hidden: "translate-x-[100%] opacity-0",
-                    }}
+            {/* Modal Header Content */}
+            <TEModalHeader className={headerClasses}>
+                <h5 className={headerTitleClasses}>
+                    {title}
+                </h5>
+
+                {/* Close button */}
+                <button type="button" onClick={() => setShowModal(false)} aria-label="Close"
+                    className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
                 >
-                    <TEModalContent>
+                    {/* Cross Icon Svg */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </TEModalHeader>
 
-                        {/* Modal Header Content */}
-                        <TEModalHeader className={headerClasses}>
-                            <h5 className={headerTitleClasses}>
-                                {title}
-                            </h5>
+            {/* Modal Body Content */}
+            <TEModalBody>
+                {children}
+            </TEModalBody>
 
-                            {/* Close button */}
-                            <button type="button" onClick={() => setShowModal(false)} aria-label="Close"
-                                className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-                            >
-                                {/* Cross Icon Svg */}
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </TEModalHeader>
-
-                        {/* Modal Body Content */}
-                        <TEModalBody>
-                            {children}
-                        </TEModalBody>
-
-                        {/* Modal Footer Content */}
-                        <TEModalFooter>
-                            <Button
-                                label="Close" handleClick={() => setShowModal(false)}
-                                customClasses={`${themeClass.themeBgGradientSecondary} text-white`}
-                            />
-                            <Button label="Submit" buttonType="button" handleClick={handleAction}
-                                customClasses={`${themeClass.themeBgGradientPrimary} ml-1 text-white`}
-                            />
-                        </TEModalFooter>
-
-                    </TEModalContent>
-                </TEModalDialog>
-            </TEModal>
+            {/* Modal Footer Content */}
+            { (footerActions.length > 0) && (
+                <TEModalFooter>
+                    { footerActions.map((eachAction, key) => (
+                        <span key={`feedbackModalFooterAction_${key.toString()}`}>
+                            <Button {...eachAction} />
+                        </span>
+                    ))}
+                </TEModalFooter>
+            )}
         </>
-    );
+    )
 }
